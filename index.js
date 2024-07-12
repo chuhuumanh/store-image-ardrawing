9f207a9ad3c2220d8ce37b8faac699125954e260
vandu <vandu@DESKTOP-CKTBGDM>
2024-07-12 15:14:38 +0700

    Revert "export all img path to public/paths.json"
    This reverts commit 9301a6dd2054035dcba3e1bf0aecc7abc0c7109d.

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
 const express = require("express");
-const {globSync} = require('glob');
-const fs = require('fs/promises');
 const app = express();
-const path = require('path');
-const jsonFormat = require('json-format')
+
 // Thiết lập thư mục public
 app.use(express.static("public"));
 
-app.get('/public/images/paths', async (req, res) => {
-  const pattern = 'public/**/*.{png,jpg,bmp,jpeg}';
-  const files = globSync(pattern);
-  const host = 'http://159.223.66.60:2999/'
-  var allPaths = [];
-  for(const file of files){
-    const parse = path.parse(file);
-    const fileName = parse.base;
-    const dir = path.dirname(file).split(path.sep).filter(x => x!='public');
-    const noPublic = path.dirname(file).replace(/public|\\/g, '/');
-    const fileDetails = {
-      file: path.posix.join(host, noPublic, fileName).replace('/', '//'),
-      name: parse.name.replace(/-/g, ' '),
-      time: 10,
-      standing: false
-    }
-    if(!allPaths.find(x => x.name === dir[0]))
-      allPaths.push({
-        name: dir[0],
-        icon: '',
-        list: []
-      });
-    allPaths.find(x => x.name === dir[0]).list.push(fileDetails);
-  }
-  const data = JSON.stringify(allPaths, null, 2);
-  await fs.writeFile('public/paths.json', data);
-  res.status(200).json(allPaths);
-});
-
 // Khởi động máy chủ
-const port = 8080;
+const port = 2999;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
